9df1b2122d99c949150f74a18424b5525f20cded
yazeed <yazed1332003@gmail.com>
2025-09-18 13:50:21 +0300

    Updated home component

--- client/src/features/home/home.component.ts
+++ client/src/features/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, Input, input, OnInit, signal } from '@angular/core';
 import { RegisterComponent } from '../account/register/register.component';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../../types/user';
+import { AccountService } from '../../core/_services/account.service';
 
 @Component({
   selector: 'app-home',
@@ -12,6 +13,7 @@ import { User } from '../../types/user';
 export class HomeComponent implements OnInit {
   @Input({ required: true }) membersFromApp: User[] = [];
   protected registerMode = signal(false);
+  protected accountService = inject(AccountService);
   http = inject(HttpClient);
   users: any;
   showRegister(value: boolean) {
